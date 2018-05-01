32c4e081881113630cea1d1bde93c1360b97b7a4
Christian Jacobsen <christian_biks@hotmail.com>
2018-05-01 15:17:19 +0200

    change sec list 1

--- main.tf
+++ main.tf
@@ -150,13 +150,13 @@ resource "oci_core_security_list" "demo_seclist_1" {
 
   // allow outbound tcp traffic on all ports
   egress_security_rules {
-    destination = "192.168.2.0/24"
+    destination = "192.168.1.0/24"
     protocol    = "6"         //tcp
   }
 
   // allow outbound udp traffic on a port range
   egress_security_rules {
-    destination = "192.168.2.0/24"
+    destination = "192.168.1.0/24"
     protocol    = "17"        // udp
     stateless   = true
   }
